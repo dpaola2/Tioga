cf2e9c0ed5b4448ea86c2ca92ed5187801a9cb38
Dave Paola <dpaola2@gmail.com>
2015-11-29 17:22:35 -0800

    fuck chunks

--- config/webpack/production.config.js
+++ config/webpack/production.config.js
@@ -5,18 +5,18 @@ var path = require('path');
 
 var config = module.exports = require('./main.config.js')
 
-config.output = _.merge(config.output, {
+/*config.output = _.merge(config.output, {
   path: path.join(config.context, 'public', 'assets'),
   filename: '[name]-bundle-[chunkhash].js',
   chunkFilename: '[id-bundle-chunkhash].js',
-});
+});*/
 
 config.plugins.push(
-  new webpack.optimize.CommonsChunkPlugin('common', 'common-[chunkhash].js'),
-  new ChunkManifestPlugin({
+  //new webpack.optimize.CommonsChunkPlugin('common', 'common-[chunkhash].js'),
+  /*new ChunkManifestPlugin({
     filename: 'webpack-common-manifest.json',
     manfiestVariable: 'webpackBundleManifest',
-  }),
+  }),*/
   new webpack.optimize.UglifyJsPlugin(),
   new webpack.optimize.OccurenceOrderPlugin()
 );
