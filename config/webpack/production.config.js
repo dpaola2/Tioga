594e5da14d2e07350fdf92ca0e688c360f4d1606
Dave Paola <dpaola2@gmail.com>
2015-11-29 18:36:50 -0800

    new path for prod bundle

--- config/webpack/production.config.js
+++ config/webpack/production.config.js
@@ -5,19 +5,9 @@ var path = require('path');
 
 var config = module.exports = require('./main.config.js')
 
-config.output.path = path.join(config.context, '..', '..', 'app', 'assets', 'javascripts');
-/*config.output = _.merge(config.output, {
-  path: path.join(config.context, 'public', 'assets'),
-  filename: '[name]-bundle-[chunkhash].js',
-  chunkFilename: '[id-bundle-chunkhash].js',
-});*/
+config.output.path = path.join(__dirname, 'app', 'assets', 'javascripts');
 
 config.plugins.push(
-  //new webpack.optimize.CommonsChunkPlugin('common', 'common-[chunkhash].js'),
-  /*new ChunkManifestPlugin({
-    filename: 'webpack-common-manifest.json',
-    manfiestVariable: 'webpackBundleManifest',
-  }),*/
   new webpack.optimize.UglifyJsPlugin(),
   new webpack.optimize.OccurenceOrderPlugin()
 );
