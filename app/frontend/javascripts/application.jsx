e650f318417ae6e5f8009477ee3d4dcc816169f6
Justin Tulloss <justin.tulloss@gmail.com>
2015-12-01 23:11:36 -0800

    Get rid of flash of unstyled content
    By loading all of the javascript up front, we also get all of the
    packaged styling.
    
    Long term you probably want to break the CSS out of the bundle so it
    can be downloaded separately but this is fine for now.

--- app/frontend/javascripts/application.jsx
+++ app/frontend/javascripts/application.jsx
@@ -5,7 +5,8 @@ var ReactDOM = require('react-dom');
 var BulletJournal = require('./bulletJournal');
 var Data = require('./data');
 
-ReactDOM.render(<BulletJournal />, document.getElementById('content'));
-window.Data = Data;
-window.moment = require('moment');
-
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(<BulletJournal />, document.getElementById('content'));
+    window.Data = Data;
+    window.moment = require('moment');
+});
